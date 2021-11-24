e39cdfdb51dd14447d35e0c3179c932a9b5b50f1
“VenuReddy773” <venu706@gmail.com>
2021-11-24 18:39:17 +0530

    [VenuGopalReddy] Added: Banking Cash Counter Program

---
+++ DatastructuresAndAlgorithms/DatastructuresAndAlgorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using DatastructuresAndAlgorithms.Utility;
+using System;
+
+namespace DatastructuresAndAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BankingCashCounter bank = new BankingCashCounter();
+            bank.Counter();
+        }
+    }
+}

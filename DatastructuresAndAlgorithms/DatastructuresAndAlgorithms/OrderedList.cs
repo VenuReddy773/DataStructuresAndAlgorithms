9f72d016b57bcc3ffb80c429868890d4d86951e9
“VenuReddy773” <venu706@gmail.com>
2021-11-28 21:39:46 +0530

    [VenuGopalReddy] Added: Ordered List Program

---
+++ DatastructuresAndAlgorithms/DatastructuresAndAlgorithms/OrderedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DatastructuresAndAlgorithms
+{
+    class OrderedList
+    {
+        public static LinkedList<int> List = new LinkedList<int>();
+        public static string filepath = @"C:\Users\Venu Gopal Reddy\Bridgelabz\c#\.net\DataStructuresAndAlgorithms\DataStructuresAndAlgorithms\DatastructuresAndAlgorithms\DatastructuresAndAlgorithms\Utility\Ordered.txt";
+        public static string text = File.ReadAllText(filepath);
+        public string[] num = text.Split(",");
+        List<int> numbers = new List<int>();
+        public void Read()
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                numbers.Add(int.Parse(num[i]));
+            }
+            numbers.Sort();
+            foreach (var item in numbers)
+            {
+                List.Add(item);
+            }
+            List.Display();
+            this.Write();
+        }
+        public void Search(int value)
+        {
+            int count = 0;
+            while (count < num.Length)
+            {
+                if (value == numbers[count])
+                {
+                    Console.WriteLine("Number Found!!");
+                    List.Delete(numbers[count]);
+                    this.Write();
+                    List.Display();
+                    return;
+                }
+                count++;
+            }
+            Console.WriteLine("not found\n");
+            List.Add(value);
+            this.Write();
+        }
+        public void Write()
+        {
+            string text = List.Updated();
+            File.WriteAllText(filepath, text);
+        }
+    }
+}
--- DatastructuresAndAlgorithms/DatastructuresAndAlgorithms/Program.cs
+++ DatastructuresAndAlgorithms/DatastructuresAndAlgorithms/Program.cs
@@ -10,7 +10,7 @@ namespace DatastructuresAndAlgorithms
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("\nChoose an Option : \n 1.Unordered List\n 2.Exit\n");
+                Console.WriteLine("\nChoose an Option : \n 1.Unordered List\n 2.OrderedList\n 3.Exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -20,6 +20,11 @@ namespace DatastructuresAndAlgorithms
                         unordered.Search("R");
                         break;
                     case 2:
+                        OrderedList ordered = new OrderedList();
+                        ordered.Read();
+                        ordered.Search(2);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
